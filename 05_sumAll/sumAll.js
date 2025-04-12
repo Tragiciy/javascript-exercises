e3376e8999dc2f459cbf36a63f144edfb681a373
Tragiciy <5933199+Tragiciy@users.noreply.github.com>
2025-04-12 19:25:13 +0400

    05_sumAll solve

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(...numbers) {
+    let sum = 0;
 
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] < 0 || typeof numbers[i] !== 'number' || !Number.isInteger(numbers[i])) {
+            return "ERROR";
+        }
+    }
+
+    numbers.sort((a, b) => a - b)
+    // let numbers = Array.from(array);
+    for (let i = numbers[0]; i <= numbers[numbers.length - 1]; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
