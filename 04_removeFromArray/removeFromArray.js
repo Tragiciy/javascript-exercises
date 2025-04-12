a65032a2a7bc3c8170767beab59891c8c6104edc
Tragiciy <5933199+Tragiciy@users.noreply.github.com>
2025-04-12 18:57:19 +0400

    04_removeFromArray solve

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...elementsToRemove) {
+    let massive = array;
+    for (let i = 0; i < massive.length; i++) {
+        for (let j = 0; j < elementsToRemove.length; j++) {
+            if (massive[i] === elementsToRemove[j]) {
+                massive.splice(i, 1);
+                i--;
+        }
+        }
+    }
+    return massive
 };
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
