844965eb68b00e01aab055dc7b95be867cf00f80
Tragiciy <5933199+Tragiciy@users.noreply.github.com>
2025-04-15 23:26:48 +0400

    09_palindromes solve

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
 
+const palindromes = function (string ="hello") {
+    let str = string.replace(/[^A-Za-z0-9]/g, '')
+
+    str = str.toLowerCase();
+
+    let splitStr = str.split('');
+
+    let reverseStr = splitStr.reverse();
+
+    let finalStr = reverseStr.join("")
+
+    return str === finalStr;
 };
 
 // Do not edit below this line
