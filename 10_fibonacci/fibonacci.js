a8bd218eb9763456bafde88b49288a700d468fe7
Tragiciy <5933199+Tragiciy@users.noreply.github.com>
2025-04-15 23:46:10 +0400

    10_fibonacci solve

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(input) {
+    let fib = [0, 1];
 
+    let number = Number(input);
+
+    if (number <= 1 && number >= 0) {
+        return fib[number]
+    } else if (number > 1) {
+        for (let i = 2; i <= number; i++) {
+            fib.push(fib[i-1]+fib[i-2]);
+        }
+        return fib[number];
+    } else {
+        return "OOPS"
+    }
 };
 
 // Do not edit below this line
