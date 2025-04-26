11d7d7a133c9b2adcec232ea3eebf60a23471d5f
Tragiciy <5933199+Tragiciy@users.noreply.github.com>
2025-04-26 18:41:01 +0400

    12_findTheOldest solve

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const currentYear = new Date().getFullYear();
+    let oldest = people.reduce((acc, cur) => {
+        let accAge = (acc.yearOfDeath || currentYear) - acc.yearOfBirth;
 
+        let curAge = (cur.yearOfDeath || currentYear) - cur.yearOfBirth;
+
+        if (curAge > accAge) {
+            return cur
+        } else {
+            return acc
+        }
+    }, people[0]);
+
+    return oldest
 };
 
 // Do not edit below this line
